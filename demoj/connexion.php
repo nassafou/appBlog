f34c394946225740fa4f203b0d712f4f276483cf
youssouf N'TCHIRIFOU <ntyoussouf@gmail.com>
2016-04-10 22:29:06 +0200

    commit
    demoaj

--- demoj/connexion.php
+++ demoj/connexion.php
@@ -4,20 +4,16 @@
  *Nous créons deux variables : $username et $password
  *
  */
-  $username = "sdz";
-  $password = "salut";
+ // $username = "sdz";
+  //$password = "salut";
 if(isset($_POST['username']) && isset($_POST['password'])){                        
-    if($_POST['username'] == $username && $_POST['password'] == $password){// si les infos correspondent..
-        session_start();
-        $_SESSION['user'] = $username;
-        echo "Success";
-    }
-    else{// sinon
-        echo "Failed";
-        
+    //if($_POST['username'] == $username && $_POST['password'] == $password){// si les infos correspondent..
+        //session_start();
+       // $_SESSION['user'] = $username;
+       // echo "Success";
+       echo $username;
+       
     }
     
-}
-
 
 ?>
